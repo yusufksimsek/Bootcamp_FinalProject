7e31cf5c9358a5c9a732e76e9afe6b3d6d62b2fa
yusufksimsek <yusuf.simsek2015@outlook.com>
2024-12-29 13:23:12 +0300

    Login Screen designed

--- app/src/main/java/com/example/bootcamp_finalproject/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/bootcamp_finalproject/ui/screens/LoginScreen.kt
@@ -1,10 +1,61 @@
 package com.example.bootcamp_finalproject.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.bootcamp_finalproject.ui.viewmodels.AuthViewModel
 
 @Composable
 fun LoginScreen(navController: NavController,authViewModel: AuthViewModel) {
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.fillMaxSize(),
+           verticalArrangement = Arrangement.Center,
+           horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Login Page", fontSize = 32.sp)
+        
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(value = email, onValueChange = {
+            email = it
+        }, label = { Text(text = "Email") } )
+
+        OutlinedTextField(value = password, onValueChange = {
+            password = it
+        }, label = { Text(text = "Password") } )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {  }) {
+            Text(text = "Login")
+        }
+        
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextButton(onClick = {
+
+        }) {
+            Text(text = "Don't have an account? Register")
+        }
+    }
 
 }
