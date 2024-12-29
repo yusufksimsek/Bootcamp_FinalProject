944f3dd4bd249159df41af259b9a0cf77294cf11
yusufksimsek <yusuf.simsek2015@outlook.com>
2024-12-29 14:37:46 +0300

    Login authentication tested and User will go directly MainScreen if already logged before

--- app/src/main/java/com/example/bootcamp_finalproject/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/bootcamp_finalproject/ui/screens/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.bootcamp_finalproject.ui.screens
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,15 +11,19 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.bootcamp_finalproject.ui.viewmodels.AuthState
 import com.example.bootcamp_finalproject.ui.viewmodels.AuthViewModel
 
 @Composable
@@ -26,6 +31,21 @@ fun LoginScreen(navController: NavController,authViewModel: AuthViewModel) {
 
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+
+    val authState = authViewModel.authState.observeAsState()
+    val context = LocalContext.current
+
+    LaunchedEffect(authState.value) {
+        when(authState.value){
+            is AuthState.Authenticated -> navController.navigate("mainScreen")
+            is AuthState.Error -> Toast.makeText(
+                context,
+                (authState.value as AuthState.Error).message,
+                Toast.LENGTH_SHORT
+            ).show()
+            else -> Unit
+        }
+    }
 
     Column(modifier = Modifier.fillMaxSize(),
            verticalArrangement = Arrangement.Center,
@@ -45,7 +65,9 @@ fun LoginScreen(navController: NavController,authViewModel: AuthViewModel) {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(onClick = {  }) {
+        Button(onClick = {
+            authViewModel.login(email, password)
+        }) {
             Text(text = "Login")
         }
         
