fa722e989d78ac52e2e9b1dda486050328b73efa
yusufksimsek <yusuf.simsek2015@outlook.com>
2024-12-29 13:45:41 +0300

    checkAuthStatus method created in AuthViewmodel to check user authenticated or not

--- app/src/main/java/com/example/bootcamp_finalproject/ui/viewmodels/AuthViewModel.kt
+++ app/src/main/java/com/example/bootcamp_finalproject/ui/viewmodels/AuthViewModel.kt
@@ -1,7 +1,35 @@
 package com.example.bootcamp_finalproject.ui.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class AuthViewModel : ViewModel() {
 
+    private val auth : FirebaseAuth = FirebaseAuth.getInstance()
+
+    private val _authState = MutableLiveData<AuthState>()
+    val authState : LiveData<AuthState> = _authState
+
+    init {
+        checkAuthStatus()
+    }
+
+    fun checkAuthStatus(){
+        if(auth.currentUser == null) {
+            _authState.value = AuthState.UnAuthenticated
+        } else {
+            _authState.value = AuthState.Authenticated
+        }
+    }
+
+}
+
+sealed class AuthState{
+    object Authenticated : AuthState()
+    object UnAuthenticated : AuthState()
+    object Loading : AuthState()
+
+    data class Error(val message:String) : AuthState()
 }
