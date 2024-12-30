05f0251d92fc0353c49431df4a451178b77489e7
yusufksimsek <yusuf.simsek2015@outlook.com>
2024-12-30 16:22:03 +0300

    Movies listed statically in MainScreen

--- app/src/main/java/com/example/bootcamp_finalproject/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/bootcamp_finalproject/ui/screens/MainScreen.kt
@@ -1,11 +1,17 @@
 package com.example.bootcamp_finalproject.ui.screens
 
+
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.bootcamp_finalproject.ui.screens.components.Movies
 import com.example.bootcamp_finalproject.ui.screens.components.UpcomingMovies
 
 @Composable
@@ -14,8 +20,19 @@ fun MainScreen() {
         modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues(8.dp)
     ) {
+        item { UpcomingMovies() }
+
         item {
-            UpcomingMovies()
+            Text(
+                text = "Check Movies",
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                modifier = Modifier.padding(4.dp)
+            )
+        }
+
+        items(10) { index ->
+            Movies()
         }
     }
 }
