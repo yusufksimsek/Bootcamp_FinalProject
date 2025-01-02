b394d556a8921e29f8386446c5c74af5625bc15f
yusufksimsek <yusuf.simsek2015@outlook.com>
2025-01-02 15:53:35 +0300

    LaunchedEffect added into CartScreen with getMovieCart method.

--- app/src/main/java/com/example/bootcamp_finalproject/ui/screens/CartScreen.kt
+++ app/src/main/java/com/example/bootcamp_finalproject/ui/screens/CartScreen.kt
@@ -1,55 +1,43 @@
 package com.example.bootcamp_finalproject.ui.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.MutableLiveData
-import androidx.navigation.NavController
 import com.example.bootcamp_finalproject.R
-import com.example.bootcamp_finalproject.data.entity.Movie_Cart
 import com.example.bootcamp_finalproject.ui.viewmodels.CartViewModel
-import com.google.gson.Gson
 import com.skydoves.landscapist.glide.GlideImage
-import kotlinx.coroutines.launch
 
 @Composable
 fun CartScreen(cartViewModel: CartViewModel) {
 
     val moviesList = cartViewModel.moviesList.observeAsState(listOf())
+
+    LaunchedEffect(key1 = true) {
+        cartViewModel.getMovieCart("yusuf_simsek")
+    }
 
     LazyColumn(
         modifier = Modifier
