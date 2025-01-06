d893a4b7bc0a261ed911e154af39aae24171b4b3
yusufksimsek <yusuf.simsek2015@outlook.com>
2025-01-06 17:11:49 +0300

    SearchBar background color re-designed and icon has changed.

--- app/src/main/java/com/example/bootcamp_finalproject/ui/screens/components/SearchBar.kt
+++ app/src/main/java/com/example/bootcamp_finalproject/ui/screens/components/SearchBar.kt
@@ -1,18 +1,13 @@
 package com.example.bootcamp_finalproject.ui.screens.components
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -22,11 +17,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import com.example.bootcamp_finalproject.R
+import com.example.bootcamp_finalproject.ui.theme.Colors
 
 @Composable
 fun SearchBar(
@@ -36,8 +31,8 @@ fun SearchBar(
         mutableStateOf("")
     }
 
-    Box(modifier = Modifier.padding(10.dp)) {
-
+    Box(modifier = Modifier.padding(10.dp)
+    ) {
         TextField(
             value = text,
             onValueChange = {
@@ -46,35 +41,29 @@ fun SearchBar(
             },
             maxLines = 1,
             singleLine = true,
-            textStyle = TextStyle(color = Color.Black),
+            textStyle = TextStyle(color = Colors.barTitleColor),
             colors = TextFieldDefaults.colors(
-                focusedTextColor = MaterialTheme.colorScheme.primaryContainer,
-                focusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
-                unfocusedContainerColor = MaterialTheme.colorScheme.primaryContainer,
-                disabledContainerColor = MaterialTheme.colorScheme.primaryContainer,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                disabledIndicatorColor = Color.Transparent,
-                focusedLeadingIconColor = MaterialTheme.colorScheme.primaryContainer,
-                unfocusedLeadingIconColor = MaterialTheme.colorScheme.primaryContainer,
-                focusedTrailingIconColor = Color(0xFF58647C),
-                unfocusedTrailingIconColor = Color(0xFF58647C),
+                focusedContainerColor = Colors.searchCartBackground,
+                unfocusedContainerColor = Colors.searchCartBackground,
+                focusedIndicatorColor = Colors.searchCartBackground, // Çizgiyi kaldırıyoruz
+                unfocusedIndicatorColor = Colors.searchCartBackground // Çizgiyi kaldırıyoruz
             ),
             trailingIcon = {
                 IconButton(onClick = {
                     onSearch(text)
                 }) {
                     Icon(
-                        imageVector = Icons.Outlined.Search,
-                        contentDescription = "Search Icon"
+                        modifier = Modifier.size(20.dp),
+                        painter = painterResource(id = R.drawable.search),
+                        contentDescription = "Search Icon",
+                        tint = Colors.barTitleColor
                     )
                 }
             },
             modifier = Modifier
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(12.dp))
-                .shadow(15.dp, spotColor = Color.White)
-                .background(MaterialTheme.colorScheme.primaryContainer)
+                .background(Colors.searchCartBackground)
                 .padding(horizontal = 10.dp)
         )
     }
